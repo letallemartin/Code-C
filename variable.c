a22d3216c12b3e1e8f9a97df1ba14c13abff449c
letallemartin <letallemartin@gmail.com>
2026-08-09 18:48:24 +0200

    first commit

---
+++ variable.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(void)
+{
+int entier = 10;
+float flottant = 3.14;
+char caractere = 'A';
+
+printf("%d\n", entier);
+printf("%.2f\n", flottant);
+printf("%c\n", caractere);
+printf("Entier : %d, Flottant : %.2f, Char : %c\n"
+    ,entier, flottant, caractere);
+return 0;
+}
