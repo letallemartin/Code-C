5b5e9ba44d603316c3a2dbc306f99974720a10db
letallemartin <letallemartin@gmail.com>
2026-08-10 12:47:35 +0200

    3 commit

---
+++ tp1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+float nombre;
+printf("entrez la temp en degrés");
+scanf("%f\n", &nombre);
+printf("%f\n", nombre*33.8);
+
+return 0;
+}
--- variable.c
+++ variable.c
@@ -6,7 +6,7 @@ float flottant = 3.14;
 char caractere = 'A';
 
 printf("%d\n", entier);
-printf("%.2f\n", flottant);
+printf("%.2f\n", flottant); //.2 sert uniquement a indiquer 2 chiffre aprés la virgule
 printf("%c\n", caractere);
 printf("Entier : %d, Flottant : %.2f, Char : %c\n"
     ,entier, flottant, caractere);
