7cfc22371bdaabc6f8f5d79fe91a711042354ac3
letallemartin <letallemartin@gmail.com>
2026-08-11 12:06:19 +0200

    boucle while/do while/for

---
+++ boucle.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void)
+{
+for (int i = 0; i < 5; i++);
+    {
+    printf("%d\n", i)
+    }
+}
+// control c to stop programme if boucle infinit
+
+// int i = 0;
+
+// while (i<5) 
+//     {
+//         printf("%d\n", i);
+//         i = i + 1;
+//     }
+
+// do (i<5) 
+//     {
+//         printf("%d\n", i);
+//         i = i + 1;
+//     } while (i<5);
+
+// i++ ++i --i i--
