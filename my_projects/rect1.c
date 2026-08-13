427c332b255b2e45cc6920d5c8f1b5a5fa884bb4
letallemartin <letallemartin@gmail.com>
2026-08-13 19:58:21 +0200

    debut sdl

---
+++ my_projects/rect1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <SDL3/SDL.h>
+#include <stdbool.h>
+
+int main(void)
+{
+    int width = 100;
+    int height = 100;
+    bool isopen = true;
+    
+    while (isopen == true)
+    {
+                     
+        
+    }
+    return 0;
+}
