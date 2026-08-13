fdec882c2e758e32308061c99318be6aaa08f3cd
letallemartin <letallemartin@gmail.com>
2026-08-13 21:23:42 +0200

    malloc 1

---
+++ memoir.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int tabl[5];
+    int taille = 5;
+    // printf("*%lu\n", sizeof(int));
+    int *ptr = malloc(taille & sizeoff(*ptr)); //ou int *ptr = malloc(taille & sizeoff(int));
+    if (ptr == NULL)
+    {
+    printf("error !!!\n");
+    return 1;
+    }
+    ptr[0] = 0;
+    ptr[1] = 1;
+    ptr[2] = 2;
+    ptr[3] = 3;
+    ptr[4] = 4;
+    for (int i = 0; i < 5; i++)
+    {
+        printf("ptr[%d] = %d\n",i , ptr[i]);
+    }
+free(ptr);
+
+ptr = NULL;
+return 0;
+}
