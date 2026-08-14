58032d022326f7bf7dd5dd251f4fa775bad7affc
letallemartin <letallemartin@gmail.com>
2026-08-14 11:40:39 +0200

    just verif erreur à éviter

--- memoir/erreur.c
+++ memoir/erreur.c
@@ -3,6 +3,16 @@
 
 int main(void)
 {
+int taille;
 
+char *ptr = malloc(taille * sizeof(*ptr));
+
+if (ptr = NULL)
+{
+    printf("error !!!\n");
+    return 1;
+}
+free(ptr);
+ptr = NULL;
 return 0;
 }
