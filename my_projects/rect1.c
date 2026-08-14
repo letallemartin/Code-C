402bf2111d3f068bb82ede687578b346c114c1a2
letallemartin <letallemartin@gmail.com>
2026-08-14 14:25:57 +0200

    rectangle show

--- my_projects/rect1.c
+++ my_projects/rect1.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
 #include <SDL3/SDL.h>
 #include <stdbool.h>
+#define WIDTH 800
+#define HEIGHT 600
 
 int main(void)
 {
-    int width = 800;
-    int height = 600;
     bool isopen = true;
     SDL_Event event;
-
+    SDL_FRect rect = {100.0f, 150.0f, 200.0f, 200.0f};
     if (!SDL_Init(SDL_INIT_VIDEO))
     {
         printf("Erreur lors de l'initialisation de SDL : %s\n", SDL_GetError());
         return 1;
     }
-    SDL_Window *fenetre = SDL_CreateWindow("ma fenetre SDL3", width, height, 0);
+    SDL_Window *fenetre = SDL_CreateWindow("ma fenetre SDL3", WIDTH, HEIGHT, 0);
+    SDL_Renderer *renderer = SDL_CreateRenderer(fenetre, NULL);
 
     if (fenetre == NULL)
     {
@@ -22,7 +23,7 @@ int main(void)
         SDL_Quit();
         return 1;
     }
-    while (isopen == true)
+    while (isopen)
     {
         while (SDL_PollEvent(&event))
         {
@@ -31,9 +32,18 @@ int main(void)
                 isopen = false;
             }
         }
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_RenderClear(renderer);
+
+        SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+        SDL_RenderRect(renderer, &rect);
+
+        SDL_RenderPresent(renderer);
+        
     }
     SDL_DestroyWindow(fenetre);
     SDL_Quit();
     return 0;
 }
+
 //gcc rect1.c -I . -L . -lSDL3 -o rect1.exe
