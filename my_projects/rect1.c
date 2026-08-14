d936edca6d5968c6ce46f367911bac5535ae93ee
letallemartin <letallemartin@gmail.com>
2026-08-14 12:29:56 +0200

    open window

--- my_projects/rect1.c
+++ my_projects/rect1.c
@@ -4,14 +4,36 @@
 
 int main(void)
 {
-    int width = 100;
-    int height = 100;
+    int width = 800;
+    int height = 600;
     bool isopen = true;
-    
+    SDL_Event event;
+
+    if (!SDL_Init(SDL_INIT_VIDEO))
+    {
+        printf("Erreur lors de l'initialisation de SDL : %s\n", SDL_GetError());
+        return 1;
+    }
+    SDL_Window *fenetre = SDL_CreateWindow("ma fenetre SDL3", width, height, 0);
+
+    if (fenetre == NULL)
+    {
+        printf("erreur lors de la creation de la fenetre: %s\n", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    }
     while (isopen == true)
     {
-                     
-        
+        while (SDL_PollEvent(&event))
+        {
+            if (event.type == SDL_EVENT_QUIT)
+            {
+                isopen = false;
+            }
+        }
     }
+    SDL_DestroyWindow(fenetre);
+    SDL_Quit();
     return 0;
 }
+//gcc rect1.c -I . -L . -lSDL3 -o rect1.exe
