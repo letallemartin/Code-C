9d32d2f3e628f7d8db9c5cfe0fff64df19468fb7
letallemartin <letallemartin@gmail.com>
2026-08-15 09:52:28 +0200

    simple use of pointeur and void

---
+++ memoir/ponteur4.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int x = 42;
+    int *ptr = & x;
+
+    printf("%d\n", x); // 42 — la valeur
+    printf("%d\n",(void*)ptr); // 0x7ff… — l'adresse
+    printf("%d\n", *ptr); // valeur à l'adresse
+    // (déréférencement)
+
+    *ptr = 100; // Modifie x à travers le pointeur
+    printf("%d\n", x); // 100
+
+    // &x → adresse de x
+    // *ptr → valeur à l'adresse contenue dans ptr
+    // int *p → déclaration d'un pointeur vers int
+    
+return 1;
+}
