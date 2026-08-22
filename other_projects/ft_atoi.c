fc39bcfbdb87c1d8a698e572c0f1eb2e84af950f
letallemartin <letallemartin@gmail.com>
2026-08-22 11:06:51 +0200

    ft_atoi 1/2

--- other_projects/ft_atoi.c
+++ other_projects/ft_atoi.c
@@ -3,28 +3,34 @@ int	ft_atoi(char *str)
 {
 	int	i;
 	int nbr;
+	int signe;
 	i = 0;
 	nbr = 0;
+	signe = 1;
 	while (str[i] != '\0')
 	{
 		if (str[i] > 47 && str[i] < 58)
 		{
 			nbr = nbr * 10 + (str[i] - '0');
 		}
-		else if (str[i] < 48 || str[i] > 57)
+		else if (str[i] == 45)
+		{
+			signe = -signe;
+		}
+		else if (str[i] < 48 || str[i] > 57 && str[i] != 45)
 		{
 			printf("%d", nbr);
-			return (nbr);
+			return (nbr*signe);
 		}
 		i++;
 	}
 	printf("%d", nbr);
-	return (nbr);
+	return (nbr*signe);
 }
 
 int main(void)
 {
-	char str[10] = "55he542";
+	char str[10] = "-22he542";
 	ft_atoi(str);
 	return (0);
 }
